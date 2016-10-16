26ed3bf595b1e1b517386f432f1e2bf81c59bbf8
krishna122 <kchaitanya122@gmail.com>
2016-10-16 06:11:46 +0000

    new learn cookbook added

---
+++ cookbooks/learn/metadata.rb
@@ -0,0 +1,7 @@
+name 'learn'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures learn'
+long_description 'Installs/Configures learn'
+version '0.1.0'
---
+++ cookbooks/learn/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: learn
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
